14cc01910bbeeb215a8a7787fd2bf8cbd147141a
olaaaiap <01opikabea@gmail.com>
2025-12-29 18:45:38 +0100

    Codigo y readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Aproximación de pi utilizando Monte Carlo
+
+En este problema tenemos un círculo de radio a y un cuadrado de a**2. Según la hipótesis la division entre la cantidad de pelotas que caen dentro del circulo dividido por las que han caido dentro del cuadrado deberia de resultar en una aproximación d epi.
+
+Para llevar a cabo el problema hemos definido unos areas y distancias, como se pueden ver en la siguiente imagen.
+
+
+
+![Definición de tabla](https://github.com/user-attachments/assets/2d58bcdb-e0f5-4c6a-b40b-9cd6543996a4)
+
+Esta definición de puntos relevantes nos ha servido como punto de partida a la hora de implementar la aproximación.
--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def main():
-    print("Hello from fuma-lab1!")
+
+from numpy.random import default_rng
+import numpy as np
+
+def experimento(x,y,a):
+    """
+    simular el lanzamiento de un punto con x de 0 a 4a
+    e y de 0 a 2a de forma totalmente aleatoria
+
+    :param rng: generador de numero aleatorios
+    """
+    
+    if(x>2*a):
+        #Puede estar dentro del circulo, comprobar como en clase
+        if((x-3*a)**2 + (y-a)**2 <= a**2):
+            return 1 
+        else:
+            return 0
+    else:
+        #Comprobar si esta dentro del cuadrado
+        if(x>a/2 and x<1.5*a and y>a/2 and y<1.5*a):
+            return 2
+        else:
+            return 0
+    
+N = int(1e8)    
+a = 80 #80cm
+rng = default_rng(20) #Con la misma semilla nos da el mismo resultado pero mas eficiente
+# var_sto = rng.uniform(-1, 1, size=(N, 2))
+x = rng.uniform(0, 4*a, size=N)
+y = rng.uniform(0, 2*a, size=N)
+
+# combinamos en un array de N filas y 2 columnas
+var_sto = np.column_stack((x, y))
+resultados = [experimento(var_sto[i, 0], var_sto[i, 1],a) for i in range(N)]
+
+n_circulo = sum(1 for r in resultados if r == 1)
+n_cuadrado = sum(1 for r in resultados if r == 2)
+pi_aprox = n_circulo / n_cuadrado
 
 
-if __name__ == "__main__":
-    main()
+
+print("versión eficiente   ", pi_aprox)
