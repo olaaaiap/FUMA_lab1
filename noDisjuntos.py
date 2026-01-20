a392c2937e189ab5318ffd5ce8e9c0cbe2d9eef3
LonchaDeXope <142729532+LonchaDeXope@users.noreply.github.com>
2026-01-20 13:39:59 +0100

    Apartado No Disjuntos

---
+++ noDisjuntos.py
@@ -0,0 +1,57 @@
+
+from numpy.random import default_rng
+import numpy as np
+
+######################################################################################
+#¿DEBEN SER LAS SECCIONES DISJUNTAS?
+######################################################################################
+
+def checkForPointInside(x,y,a):
+
+    #Defino el cuadrado, centrado en el punto (a,a)
+    squareArea = (x>a/2 and x<1.5*a and y>a/2 and y<1.5*a)
+
+    #Defino el círculo, desplazado ligeramente para generar una intersección entre ambos
+    circleArea = ((x - 1.5*a)**2 + (y - a)**2 <= a**2)
+
+    return (1 if circleArea else 0) + (2 if squareArea else 0)
+
+def randomPointInSurface(rng):
+    x = rng.uniform(0, 40, size=N) 
+    y = rng.uniform(0, 80, size=N)
+
+    return np.column_stack((x, y))
+
+
+aValues = [2,5,10,15]    
+N = int(1e7) 
+seed = default_rng(20)
+
+for a in aValues:    
+
+    surface = randomPointInSurface(seed)
+    results = list(map(lambda v: checkForPointInside(v[0], v[1], a), surface))
+
+    results = np.array(results)
+
+    Ecircle = np.sum(results == 1)
+    Esquare = np.sum(results == 2)
+    Eintersection = np.sum(results ==3)
+
+    piValue = (Ecircle+ Eintersection) / (Esquare + Eintersection)
+    print("Para a = ",a, " contando la intersección para ambos, pi:" ,piValue)
+
+    piValue = (Ecircle) / (Esquare)
+    print("Para a = ",a, " excluyendo la interseccion para el círculo, pi:" ,piValue)
+
+print("")
+print("---------------------- JUSTIFICACION ----------------------")
+print("La justificación más precisa es que depende. Existen dos casos:")
+print("En el caso de que no sean disjuntos, y las pelotas que entren en " \
+" la intersección de ambas áreas cuenten como contenidas tanto en una como en " \
+"otra, la proporción real de las áreas se mantiene intacta, haciendo que la " \
+"estimación sea consistente ")
+print("Sin embargo, en el caso de que no sean disjuntos y las pelotas que entren en " \
+" la intersección de ambas áreas  no cuenten como contenidas en ninguna, establecería " \
+"un sesgo muy grande en favor al círculo, porque el cuadrado comprende más porcentaje de " \
+"su área en la intersección, aumentando el valor de pi de forma artificial")
